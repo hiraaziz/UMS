ce2b65a68de6bb4925f4790ab54c4c3b6ca5d315
hiraaziz <hiraaziz1998@gmail.com>
2022-12-17 16:06:25 +0500

    Create Teacher.js

---
+++ Teacher.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Person_js_1 = require("./Person.js");
+class Teacher extends Person_js_1.Person {
+    constructor(name, id) {
+        super(name, id);
+    }
+}
