4e3cb7c58df02ccadc83dbf5c20ad3319a63a97f
hiraaziz <hiraaziz1998@gmail.com>
2022-12-17 16:06:50 +0500

    Create Course.ts

---
+++ Course.ts
@@ -0,0 +1,10 @@
+export class Course{
+
+    course_id:string
+    course_name:string
+
+    constructor(course_id:string,course_name:string){
+        this.course_id = course_id,
+        this.course_name = course_name
+    }
+}
