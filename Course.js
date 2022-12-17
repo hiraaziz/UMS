8a6cfad1884e9cbe279f048d83b168351a6d0307
hiraaziz <hiraaziz1998@gmail.com>
2022-12-17 16:06:53 +0500

    Create Course.js

---
+++ Course.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Course = void 0;
+class Course {
+    course_id;
+    course_name;
+    constructor(course_id, course_name) {
+        this.course_id = course_id,
+            this.course_name = course_name;
+    }
+}
+exports.Course = Course;
