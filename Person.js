b2774e80fb1d2c0f427a12e0893f9e3acd8585de
hiraaziz <hiraaziz1998@gmail.com>
2022-12-17 16:06:32 +0500

    Create Person.js

---
+++ Person.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Person = void 0;
+class Person {
+    name;
+    ID;
+    constructor(name, ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+}
+exports.Person = Person;
