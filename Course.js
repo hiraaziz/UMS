3b56371740a981283e656c04ac0cb30e5e4d2cc4
hiraaziz <hiraaziz1998@gmail.com>
2023-02-14 00:53:22 +0500

    Update Course.js

--- Course.js
+++ Course.js
@@ -1,12 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Course = void 0;
-class Course {
+export class Course {
     course_id;
     course_name;
+    Student;
+    instructor;
     constructor(course_id, course_name) {
-        this.course_id = course_id,
-            this.course_name = course_name;
+        this.course_id = course_id;
+        this.course_name = course_name;
+        this.Student = [];
+        this.instructor = [];
+    }
+    addStudent(Student) {
+        this.Student.push(Student);
+    }
+    setInstructor(instructor) {
+        this.instructor.push(instructor);
     }
 }
-exports.Course = Course;
