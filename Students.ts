fb5811b59dbefae5dc532110e5e36fa8e0f2697f
hiraaziz <hiraaziz1998@gmail.com>
2023-02-14 00:53:04 +0500

    Update Students.ts

--- Students.ts
+++ Students.ts
@@ -1,13 +1,19 @@
 import { Person } from "./Person.js"
 import { Course } from "./Course.js"
 
-class Students extends Person{
+export class Students extends Person{
 
     course:Course[]
 
-    constructor(name:string,id:number){
+    constructor(id:string,name:string){
 
-        super(name,id)
+        super(id,name)
         this.course = []
     }
+
+    addCourse(Courses:Course){
+        this.course.push(Courses);
+        Courses.addStudent(this)
+    }
+    
 }
