5373ad72f8bf55d0ca26ce7f5df277968a58d5d3
hiraaziz <hiraaziz1998@gmail.com>
2023-02-14 00:53:13 +0500

    Update University.ts

--- University.ts
+++ University.ts
@@ -1,3 +1,40 @@
-class University{
-    
-}
+import { Instructor } from './Instructor.js';
+import { Students } from "./Students.js";
+import { Course } from "./Course.js";
+
+export class University{
+
+    Students:Students[]
+    Course:Course[]
+    Instructor:Instructor[]
+
+    constructor(){
+        this.Students = [];
+        this.Course = [];
+        this.Instructor = []
+    }
+
+    addStudents(Student:Students){
+        this.Students.push(Student);
+    }
+
+    addCourses(Course:Course){
+        this.Course.push(Course)
+    }
+
+    addInstructor(instructor:Instructor){
+        this.Instructor.push(instructor)
+    }
+
+    showStudents(){
+        this.Students.map(stu=>console.log(stu))
+    }
+
+    showCourses(){
+        this.Course.map(course => console.log(course))
+    }
+
+    showInstructor(){
+        this.Instructor.map(tchr=>console.log(tchr))
+    }
+} 
