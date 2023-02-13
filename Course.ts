bbd48f45e057cedc9c5f554af81280ebbcdfc6ba
hiraaziz <hiraaziz1998@gmail.com>
2023-02-14 00:53:19 +0500

    Update Course.ts

--- Course.ts
+++ Course.ts
@@ -1,10 +1,25 @@
+import { Students } from "./Students.js"
+import { Instructor} from './Instructor.js'
+
 export class Course{
 
     course_id:string
     course_name:string
+    Student:Students[]
+    instructor:Instructor[]
 
     constructor(course_id:string,course_name:string){
-        this.course_id = course_id,
-        this.course_name = course_name
+        this.course_id = course_id;
+        this.course_name = course_name;
+        this.Student=[]
+        this.instructor=[]
+    }
+
+    addStudent(Student:Students){
+        this.Student.push(Student)
+    }
+
+    setInstructor(instructor:Instructor){
+        this.instructor.push(instructor)
     }
 }
