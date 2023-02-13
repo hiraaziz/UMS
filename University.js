d3a844c08c2f148fbed02231a1ea62d70d68c4e2
hiraaziz <hiraaziz1998@gmail.com>
2023-02-14 00:53:15 +0500

    Update University.js

--- University.js
+++ University.js
@@ -1,4 +1,28 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class University {
+export class University {
+    Students;
+    Course;
+    Instructor;
+    constructor() {
+        this.Students = [];
+        this.Course = [];
+        this.Instructor = [];
+    }
+    addStudents(Student) {
+        this.Students.push(Student);
+    }
+    addCourses(Course) {
+        this.Course.push(Course);
+    }
+    addInstructor(instructor) {
+        this.Instructor.push(instructor);
+    }
+    showStudents() {
+        this.Students.map(stu => console.log(stu));
+    }
+    showCourses() {
+        this.Course.map(course => console.log(course));
+    }
+    showInstructor() {
+        this.Instructor.map(tchr => console.log(tchr));
+    }
 }
